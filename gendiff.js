2325233ff633a9932ce1e08ee587b909ae8e9cbf
xocoee <141821998+xocoee@users.noreply.github.com>
2023-10-21 10:23:40 +0300

    2

--- gendiff.js
+++ gendiff.js
@@ -4,7 +4,14 @@ const { Command } = require('commander');
 const program = new Command();
 
 program
-    .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
+    .option('-V, --version', 'output the version number')
+    //.option('-h, --help', 'output usage information')
+    .option('-f, --format <type>', 'output format')
+    .arguments('<filepath1> <filepath2>');
 
 program.parse();
+
+const options = program.opts();
+const version = '0.0.1'
+if (options.version) console.log(version);
